4207ced48eef9cd3519c2d131ed340b1103a6fe4
hafke88 <hafke88@gmail.com>
2021-07-02 14:23:14 -0400

    latest

---
+++ Basic JavaScript Projects/Project2_functions/JS/operator_assignment.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence= "I am learning ";
+    sentence += " a lot from this book!"; //using the operater to concatenate a string
+    document.getElementById("Concatenate").innerHTML= sentence; //ID name referencing 'Concatenate' given in html file
+}
