cc1a1f117c5d884a638e457df31784b02b2f38ca
hafke88 <hafke88@gmail.com>
2021-07-02 13:13:44 -0400

    latest

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,15 @@
-alert("Hello, World!");
+window.alert("Hello,World!"); //window alert 
+
+var A="This is a string";
+window.alert(A); //window alert with variable
+
+document.write("I said to him,\"Don't do it!\"") //Strings
+
+var A=("\"Be the change you want to see in the world\""+"-Ghandi")
+document.write(A) //Variable with strings and Concatenating
+
+var Family="Welcome to the Hafke Residence";
+var Family=Family.fontcolor("orange");
+document.write(Family); //Assigning color to variable
+
+document.write(3+3); //This in an expression that will display with the answer 6
