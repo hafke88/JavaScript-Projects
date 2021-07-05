c90da59f9c4214430b908bf4abe96be415c150e2
hafke88 <hafke88@gmail.com>
2021-07-05 19:12:26 -0400

    latest

--- Basic JavaScript Projects/Project5_comarisons_type_coercion/JS/example.js
+++ Basic JavaScript Projects/Project5_comarisons_type_coercion/JS/example.js
@@ -1,3 +1,11 @@
 function my_function(){
-    document.getElementById("test").innerHTML=isNaN('007');
+    document.getElementById("test").innerHTML=0/0;
+}
+
+function string(){
+    document.getElementById("right").innerHTML=isNaN('This is a string');
+}
+
+function number(){
+    document.getElementById("wrong").innerHTML=isNaN('007');
 }
--- Basic JavaScript Projects/Project5_comarisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comarisons_type_coercion/JS/main.js
@@ -1,9 +1,13 @@
-document.write(typeof "Color"); //output string
+document.write(typeof "Color <br>"); //output string
 
-document.write(typeof 2); //output number
+document.write(typeof 2 + "<br>"); //output number
 
-document.write("20"+8); //coercion
+document.write("20"+8 + "<br>"); //coercion
 
-document.write(2E310); //infinity
+document.write(2E310 + "<br>"); //infinity
 
-document.write(-2E310); //-infinity
+document.write(-2E310 + "<br>"); //-infinity
+
+document.write(15>2)+ "<br>"; //boolean, true statement
+
+document.write(12<2)+ "<br>"; //boolean, false statement
